0fc428a30a6d0df4cdcea4d0b7b5cc5389e295ff
Ivo Georgiev <ivogeorg@gmail.com>
2024-05-01 04:45:39 +0000

    server node wrapped in cpp class

--- src/bb8_move_circle_class.cpp
+++ src/bb8_move_circle_class.cpp
@@ -1,6 +1,10 @@
+#include "my_custom_srv_msg_pkg/MyCustomServiceMessage.h"
+#include "my_custom_srv_msg_pkg/MyCustomServiceMessageRequest.h"
+#include "my_custom_srv_msg_pkg/MyCustomServiceMessageResponse.h"
+
 #include <geometry_msgs/Twist.h>
+#include <ros/duration.h>
 #include <ros/ros.h>
-#include <std_srvs/Empty.h>
 
 class MoveBB8 {
 
@@ -18,9 +22,9 @@ public:
   geometry_msgs::Twist vel_msg;
 
   MoveBB8() {
-    my_service = nh_.advertiseService("/move_bb8_in_circle",
+    my_service = nh_.advertiseService("/move_bb8_in_circle_with_timeout",
                                       &MoveBB8::my_callback, this);
-    ROS_INFO("The Service /move_bb8_in_circle is READY");
+    ROS_INFO("The Service /move_bb8_in_circle_with_timeout is READY");
     vel_pub = nh_.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
   }
 
@@ -30,11 +34,20 @@ public:
     vel_pub.publish(vel_msg);
   }
 
-  bool my_callback(std_srvs::Empty::Request &req,
-                   std_srvs::Empty::Response &res) {
-    ROS_INFO("The Service /move_bb8_in_circle has been called");
+  void stop() {
+    vel_msg.linear.x = 0.0;
+    vel_msg.angular.z = 0.0;
+    vel_pub.publish(vel_msg);
+  }
+
+  bool my_callback(my_custom_srv_msg_pkg::MyCustomServiceMessageRequest &req,
+                   my_custom_srv_msg_pkg::MyCustomServiceMessageResponse &res) {
+    ROS_INFO("The service /move_bb8_in_circle_with_timeout has been called");
     move_in_circle();
-    ROS_INFO("Finished service /move_bb8_in_circle");
+    ros::Duration(req.duration).sleep();
+    stop();
+    res.success = true;
+    ROS_INFO("Finished service /move_bb8_in_circle_with_timeout");
     return true;
   }
 };
